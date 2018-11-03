0b13e1a85c9f96e71e7cdd7ddb94ce8b152439c5
Rohan <taogmaog@gmail.com>
2018-11-03 16:52:25 +0530

    y

--- Assignment 2/cnf.py
+++ Assignment 2/cnf.py
@@ -19,12 +19,33 @@ for x in range(1,clauses+1):
 
 #print(Clause)
 p = [0 for i in range(literals)]
+k = []
 
 def recb(i):
+    print (" i is"+str(i))
+    print (p)
     if(i==clauses):
-        return p
+        print ("hi")
+        k.append(p.copy())
+        return -1
+
 
     for n in Clause[i]:
+        j = p[abs(n)-1]
+        if p[abs(n)-1]*n <0:
+            continue
+        else:
+            p[abs(n)-1] = n/abs(n)
+            q = recb(i+1)
+            if q == -1:
+                return -1
+            p[abs(n)-1] = j
+
+
+recb(0)
+print (p)
+#ans = recb(0)
+"""print(ans)
         k =n
         if k*p[abs(k)-1]<0:
             continue
@@ -41,8 +62,4 @@ def recb(i):
                 if q==-1:
                     continue
                 return q
-        return -1
-
-
-ans = recb(0)
-print(ans)
+    return -1"""
